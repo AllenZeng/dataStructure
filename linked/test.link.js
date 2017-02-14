9360922a294e5f6c02f9d8b6d9eb86f9a22f4095
allenzeng <zengjun_cm@sina.com>
2017-02-14 16:02:06 +0800

    add link structure

---
+++ linked/test.link.js
@@ -0,0 +1,7 @@
+var LList = require('../6.linked.js');
+
+var cities = new LList();
+cities.insert('Conway', 'head');
+cities.insert('Russellville', 'Conway');
+cities.insert('Alma', 'Russellville');
+cities.display();
